1a5b623832675b78e58e2e64656da00668252bba
Gianfranco Semeraro <scuolasemerarogianfranco@gmail.com>
2020-12-22 20:54:03 +0100

    Python File per l'estrazione dei frame. Funziona, ma bisogna solo decidere :  1. Dove salvare i frame  2. E se rendere tutto automatico o fare delle mini funzioni adattabili ad ogni dataset in singolo  3. Cancellare le print/input inutili

--- EstrazioneFrameVideos.py
+++ EstrazioneFrameVideos.py
@@ -4,38 +4,54 @@ import os
 print(cv2.__version__)
 
 
-def frame_capture(file, save_path):
+def frame_capture(file, save_path, count):
     vidcap = cv2.VideoCapture(file)
     success, image = vidcap.read()
-    print(vidcap)
+    # print(vidcap)
     # print(image.shape)
     # global count
-    count = 0
+    # count = 0
     success = True
     while success:
         cv2.imwrite(save_path % count, image)  # save frame as JPEG file
         success, image = vidcap.read()
-        print('Read a new frame: ', success)
+        # print('Read a new frame: ', success)
         count += 1
-        print(count)
+        # print(count)
+    return count
 
 """
 Da aggiustare XD
 """
 
-def main():
-    path_video = "DataSet/UCF-101/ApplyEyeMakeup"
-    video_format = ".avi"
-    path_to_save = "hit%d.jpg"
-    for file in os.listdir(path_video):
-        if file.endswith(video_format):
-            path = os.path.join(path_video, file)
-            frame_capture(path, path_to_save)
+def main_UCF101():
+    # Siamo nel dataset UFC101
+    DataSet_UFC101 = "DataSet/UCF-101"
+    for class_video in os.listdir(DataSet_UFC101):
+        path_video = os.path.join(DataSet_UFC101, class_video) # Esempio "DataSet/UCF-101/ApplyEyeMakeup"
+        # Count serve per poter inserire tutti i frame senza sovrascizioni
+        count = 0
+        initial_id_video = 'g01'
+        video_format = ".avi" # Abbastanza inutile questo controllo, ma non si sa mai
+        for file in os.listdir(path_video):
+            if file.endswith(video_format):
+                # Ogni file video genererà una cartella con lo stesso suo nome con all'interno i suoi frame.
+                id_video = file.split("_")[2]
+                n_clip = file.split("_")[3].split(".")[0]
+                if n_clip == "c01":
+                    count = 0
+                folder_name = file[0:len(file)-len(video_format + "_c00")]
+                # creo la cartella
+                os.makedirs(os.path.join(path_video,folder_name), exist_ok = True)
+
+                path_to_save = os.path.join(path_video,folder_name,id_video + "_frame%d.jpg")
+                path = os.path.join(path_video, file)
+                count = frame_capture(path, path_to_save, count)
 
 input('stop2')
 if __name__ == "__main__":
     input('stop')
     print("Iniziamo")
-    main()
+    main_UCF101()
     print("Fine")
 
