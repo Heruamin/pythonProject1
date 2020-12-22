f2fed33fa8454cc5c1aae96f0dd5cd6acc2f8cad
Gianfranco Semeraro <scuolasemerarogianfranco@gmail.com>
2020-12-22 21:26:53 +0100

    Python File per l'estrazione dei frame. Funziona, ma bisogna solo decidere :  1. Dove salvare i frame  2. E se rendere tutto automatico o fare delle mini funzioni adattabili ad ogni dataset in singolo  3. Cancellare le print/input inutili

--- EstrazioneFrameVideos.py
+++ EstrazioneFrameVideos.py
@@ -1,7 +1,11 @@
 import cv2
 import os
 
-print(cv2.__version__)
+"""
+Serve avere la cartella DataSet con la struttura :
+   - UCF101 -> folder -> video.avi
+   - UT-Interaction -> folder -> video.avi   
+"""
 
 
 def frame_capture(file, save_path, count):
@@ -20,19 +24,15 @@ def frame_capture(file, save_path, count):
         # print(count)
     return count
 
-"""
-Da aggiustare XD
-"""
 
-def main_UCF101():
+def ucf101():
     # Siamo nel dataset UFC101
     DataSet_UFC101 = "DataSet/UCF-101"
     for class_video in os.listdir(DataSet_UFC101):
-        path_video = os.path.join(DataSet_UFC101, class_video) # Esempio "DataSet/UCF-101/ApplyEyeMakeup"
+        path_video = os.path.join(DataSet_UFC101, class_video)  # Esempio "DataSet/UCF-101/ApplyEyeMakeup"
         # Count serve per poter inserire tutti i frame senza sovrascizioni
         count = 0
-        initial_id_video = 'g01'
-        video_format = ".avi" # Abbastanza inutile questo controllo, ma non si sa mai
+        video_format = ".avi"  # Abbastanza inutile questo controllo, ma non si sa mai
         for file in os.listdir(path_video):
             if file.endswith(video_format):
                 # Ogni file video genererà una cartella con lo stesso suo nome con all'interno i suoi frame.
@@ -40,18 +40,47 @@ def main_UCF101():
                 n_clip = file.split("_")[3].split(".")[0]
                 if n_clip == "c01":
                     count = 0
-                folder_name = file[0:len(file)-len(video_format + "_c00")]
+                folder_name = file[0:len(file) - len(video_format + "_c00")]
                 # creo la cartella
-                os.makedirs(os.path.join(path_video,folder_name), exist_ok = True)
+                os.makedirs(os.path.join(path_video, folder_name), exist_ok=True)
 
-                path_to_save = os.path.join(path_video,folder_name,id_video + "_frame%d.jpg")
+                path_to_save = os.path.join(path_video, folder_name, id_video + "_frame%d.jpg")
                 path = os.path.join(path_video, file)
                 count = frame_capture(path, path_to_save, count)
 
-input('stop2')
-if __name__ == "__main__":
-    input('stop')
-    print("Iniziamo")
-    main_UCF101()
-    print("Fine")
 
+def utinteraction():
+    # Siamo nel dataset UFC101
+    DataSet_UTInteraction = "DataSet/UT-Interaction"
+    for class_video in os.listdir(DataSet_UTInteraction):
+        path_video = os.path.join(DataSet_UTInteraction, class_video)  # Esempio "DataSet/UT-Interaction/hand shake"
+        # Count serve per poter inserire tutti i frame senza sovrascizioni
+        count = 0
+        video_format = ".avi"  # Abbastanza inutile questo controllo, ma non si sa mai
+        for file in os.listdir(path_video):
+            if file.endswith(video_format):
+                # Ogni file video genererà una cartella con lo stesso suo nome con all'interno i suoi frame.
+                count = 0
+                nome = "_".join(file.split("_")[0:2])
+                folder_name = file[0:len(file) - len(video_format)]
+                # creo la cartella
+                os.makedirs(os.path.join(path_video, folder_name), exist_ok=True)
+
+                path_to_save = os.path.join(path_video, folder_name, nome + "frame%d.jpg")
+                path = os.path.join(path_video, file)
+                _ = frame_capture(path, path_to_save, count)
+
+
+def hmdb51():
+    """
+    To-Do
+    """
+    return True
+
+
+if __name__ == "__main__":
+    print("Iniziamo")
+    utinteraction()
+    ucf101()
+    hmdb51()
+    print("Fine")
