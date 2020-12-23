33dacb367b039f7dbce7a307a3424ba7b9361130
Gianfranco Semeraro <scuolasemerarogianfranco@gmail.com>
2020-12-23 22:08:09 +0100

    Python File per l'estrazione dei frame. Funziona, ma bisogna solo decidere :  1. Dove salvare i frame  2. E se rendere tutto automatico o fare delle mini funzioni adattabili ad ogni dataset in singolo  3. Cancellare le print/input inutili

--- EstrazioneFrameVideos.py
+++ EstrazioneFrameVideos.py
@@ -29,8 +29,8 @@ def ucf101():
     # Siamo nel dataset UFC101
     # Inseriamo i nomi delle cartelle che finiranno in violence
     list_violence = ["Punch", "SumoWrestling"]
-    #Inseriamo i nomi delle cartelle che finiranno in non_violence
-    list_noviolence = ["Basketball","Skiing"]
+    # Inseriamo i nomi delle cartelle che finiranno in non_violence
+    list_noviolence = ["Basketball", "Skiing"]
     # Path del dataset
     DataSet_UFC101 = "DataSet/UCF-101"
     # Path di salvataggio dei frame
@@ -66,7 +66,7 @@ def ucf101():
                 # Creo la cartella violence e noviolence e se ci sono allora no_problem
                 os.makedirs(os.path.join(DataExtract_UFC101, class_folder), exist_ok=True)
                 # i diversi frame di ogni video saranno distinguibili
-                path_to_save = os.path.join(DataExtract_UFC101, class_folder,folder_name + "_frame%d.jpg")
+                path_to_save = os.path.join(DataExtract_UFC101, class_folder, folder_name + "_frame%d.jpg")
                 # questo è il path del file video
                 path_file = os.path.join(path_video, file)
 
@@ -77,7 +77,7 @@ def utinteraction():
     # Siamo nel dataset UT-Interaction
     # Inseriamo i nomi delle cartelle che finiranno in violence
     list_violence = ["kick", "push", "punching"]
-    #Inseriamo i nomi delle cartelle che finiranno in non_violence
+    # Inseriamo i nomi delle cartelle che finiranno in non_violence
     list_noviolence = ["hand shake", "hugging", "pointting"]
     # Ci sono meno commenti perchè la logica è simile a quella della funzione ucf101
     DataSet_UTInteraction = "DataSet/UT-Interaction"
@@ -112,44 +112,45 @@ def utinteraction():
                 # in questo caso la count viene sempre fatta partire da 0 poichè sono clip diverse
                 _ = frame_capture(path_file, path_to_save, count)
 
+
 # Questa funzione lavora un po' diversamente dalle precedenti perchè si occuperà di prendere i frame, rinominarli
 # e spostarli
 def hmdb51():
     # Siamo nel dataset HMDB51
     # Inseriamo i nomi delle cartelle che finiranno in violence
     list_violence = ["kick", "sword", "shoot_gun", "punch", "hit"]
-    #Inseriamo i nomi delle cartelle che finiranno in non_violence
+    # Inseriamo i nomi delle cartelle che finiranno in non_violence
     list_noviolence = ["walk", "sit", "shake_hands", "pick", "wave"]
     # Ci sono meno commenti perchè la logica è simile a quella della funzione ucf101
     DataSet_HMDB51 = "DataSet/HMDB51"
     DataExtract_HMDB51 = "DataExtract/HMDB51"
     class_folder = " "
-    for class_video in os.listdir(DataSet_HMDB51): # (Esempio class_video = brush_hair )
+    for class_video in os.listdir(DataSet_HMDB51):  # (Esempio class_video = brush_hair )
         if class_video in list_violence:
             class_folder = "Violence"
         elif class_video in list_noviolence:
             class_folder = "NoViolence"
         else:
             continue
-        folder_video_path = os.path.join(DataSet_HMDB51,class_video) # DataSet/HMDB51/brush_hair
-        for folder_frame in os.listdir(folder_video_path): # folder_name = April_09_..._goo_0
+        folder_video_path = os.path.join(DataSet_HMDB51, class_video)  # DataSet/HMDB51/brush_hair
+        for folder_frame in os.listdir(folder_video_path):  # folder_name = April_09_..._goo_0
             # creamo il nome del video             
             video_name = folder_frame
-            
+
             # creamo le cartelle Violence e NoViolence, se esitono no problem
             os.makedirs(os.path.join(DataExtract_HMDB51, class_folder), exist_ok=True)
             # path cartella
-            path_frames = os.path.join(folder_video_path, folder_frame) 
+            path_frames = os.path.join(folder_video_path, folder_frame)
 
             path_to_save = os.path.join(DataExtract_HMDB51, class_folder)
 
-            for frames in os.listdir(path_frames): # sono nella cartella con il folder_name con i jpg
+            for frames in os.listdir(path_frames):  # sono nella cartella con il folder_name con i jpg
                 if not frames.endswith(".jpg"):
                     continue
-                path_file = os.path.join(path_frames,frames)
+                path_file = os.path.join(path_frames, frames)
                 numero_frame = frames[1:5]
-                path_to_save_file = os.path.join(path_to_save,video_name + "_" + numero_frame)
-                
+                path_to_save_file = os.path.join(path_to_save, video_name + "_" + numero_frame + ".jpg")
+
                 os.rename(path_file, path_to_save_file)
 
 
@@ -160,7 +161,7 @@ def ucfcrime():
     list_violence = ["Abuse", "Assault", "Fighting", "RoadAccidents", "Shoplifting",
                      "Arrest", "Burglary", "Robbery", "Stealing",
                      "Arson", "Explosion", "Shooting", "Vandalism"]
-    #Inseriamo i nomi delle cartelle che finiranno in non_violence
+    # Inseriamo i nomi delle cartelle che finiranno in non_violence
     list_noviolence = ["Normal_Videos_event"]
     # Path del dataset
     DataSet_UFCRIME = "DataSet/UCF-CRIME"
@@ -193,16 +194,22 @@ def ucfcrime():
                 # Creo la cartella violence e noviolence e se ci sono allora no_problem
                 os.makedirs(os.path.join(DataExtract_UFCRIME, class_folder), exist_ok=True)
                 # i diversi frame di ogni video saranno distinguibili
-                path_to_save = os.path.join(DataExtract_UFCRIME, class_folder,nome_clip + "_frame%d.jpg")
+                path_to_save = os.path.join(DataExtract_UFCRIME, class_folder, nome_clip + "_frame%d.jpg")
                 # questo è il path del file video
                 path_file = os.path.join(path_video, file)
 
                 _ = frame_capture(path_file, path_to_save, count)
 
+
 if __name__ == "__main__":
     print("Iniziamo")
+    print("UT-Interaction")
     utinteraction()
+    print("UCF101")
     ucf101()
+    print("HMDB51")
     hmdb51()
+    print("UCFCRIME")
     ucfcrime()
     print("Fine")
+    print("-----------------")
